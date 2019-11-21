dae2cc899c86a637f484c6a55e3c1648197577a5
Agchai52 <shuangzhang0809@gmail.com>
2019-11-21 12:59:19 -0500

    fix bugs

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import os
 from SynIMU2Blurry import *
 
 SynData = SynImages()
-img = cv2.imread("InputImages/000008.png", 1).astype(np.float)
+img = cv2.imread("InputImages/000323.png", 1).astype(np.float)
 img_H, img_W, _ = img.shape
 SynData.image_H = img_H
 SynData.image_W = img_W
 
-file_prefix = "000008"
+file_prefix = "000323"
 phase = "single"
 if not os.path.exists('Dataset') and phase != "single":
     os.makedirs('Dataset')
